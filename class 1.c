81fb6ccc0ea1a3d448e9729cb1abad875c98f9e7
madhavv-xd <mega89956@gmail.com>
2025-01-16 16:50:16 +0530

    second

--- class 1.c
+++ class 1.c
@@ -4,3 +4,15 @@ int main() {
   printf("Hello World!");
   return 0;
 }
+#include <stdio.h>
+
+int main() {
+  printf("Hello World!");
+  return 0;
+}
+#include <stdio.h>
+
+int main() {
+  printf("Hello World!");
+  return 0;
+}
