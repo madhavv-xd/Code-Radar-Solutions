a69d222cb8bda5a522b607e9d62173aaa0a8a526
Madhav <mega89956@gmail.com>
2025-02-18 01:47:54 +0530

    Solution to Check Leap Year

--- Check Leap Year@@6788dd63490637cfc44e1b90/code.c
+++ Check Leap Year@@6788dd63490637cfc44e1b90/code.c
@@ -5,21 +5,21 @@ int main() {
 
    // leap year if perfectly divisible by 400
    if (year % 400 == 0) {
-      printf("%d Leap Year.");
+      printf("%d Leap Year");
    }
    // not a leap year if divisible by 100
    // but not divisible by 400
    else if (year % 100 == 0) {
-      printf("Not a Leap Year.");
+      printf("Not a Leap Year");
    }
    // leap year if not divisible by 100
    // but divisible by 4
    else if (year % 4 == 0) {
-      printf("%d Leap Year.");
+      printf("%d Leap Year");
    }
    // all other years are not leap years
    else {
-      printf("%d Not a Leap Year.");
+      printf("%d Not a Leap Year");
    }
 
    return 0;
