42bbd415dd22c32d2a75c69aedc2a1749ab5f627
Madhav <mega89956@gmail.com>
2025-04-01 01:08:31 +0530

    Solution to Find the Maximum and Minimum Element in an Array

--- Find the Maximum and Minimum Element in an Array@@676c02002dbc8c3e7a280b70/code.c
+++ Find the Maximum and Minimum Element in an Array@@676c02002dbc8c3e7a280b70/code.c
@@ -1,27 +1,30 @@
-void bubbleSort(int arr[] , int size){
-    for(int i = 0; i < size - 1; i++){
-        for(int j = 0; j < size - i - 1; j++){ //reducing the search space 
-            if(arr[j] > arr[j+1]){
-                arr[j] = arr[j] ^ arr[j+1];
-                arr[j+1] = arr[j] ^ arr[j+1];
-                arr[j] = arr[j] ^ arr[j+1];
+#include <stdio.h>
+
+
+void bubbleSort(int arr[], int size) {
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - i - 1; j++) { // Reducing the search space
+            if (arr[j] > arr[j + 1]) {
+                arr[j] = arr[j] ^ arr[j + 1];
+                arr[j + 1] = arr[j] ^ arr[j + 1];
+                arr[j] = arr[j] ^ arr[j + 1];
             }
         }
     }
 }
 
-#include <stdio.h>
-
 int main() {
     int n;
     scanf("%d", &n);
+
 
     int a[n];
     for (int i = 0; i < n; i++) {
         scanf("%d", &a[i]);
     }
 
-    bubbleSort(int a[n] , int n);
-    printf("%d %d " , a[0] , a[n-1]);
+    bubbleSort(a, n);
+
+    printf("%d %d\n", a[0], a[n - 1]);
     return 0;
 }
