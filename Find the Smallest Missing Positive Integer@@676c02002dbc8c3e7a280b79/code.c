12d22258d8560edd153873e183341ccd1d2b17aa
Madhav <mega89956@gmail.com>
2025-04-01 02:46:52 +0530

    Solution to Find the Smallest Missing Positive Integer

---
+++ Find the Smallest Missing Positive Integer@@676c02002dbc8c3e7a280b79/code.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+void bubbleSort(int arr[], int size) {
+    for(int i = 0; i < size - 1; i++) {
+        for(int j = 0; j < size - i - 1; j++) { // reducing the search space 
+            if(arr[j] > arr[j+1]) {
+                arr[j] = arr[j] ^ arr[j+1];
+                arr[j+1] = arr[j] ^ arr[j+1];
+                arr[j] = arr[j] ^ arr[j+1];
+            }
+        }
+    }
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int a[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    bubbleSort(a, n);
+    
+    for(int i = 0; i < n - 1; i++){
+        if(a[i] >= 0){
+            if(a[i+1] != a[i] + 1){
+                printf("%d" , a[i] + 1);
+            }
+        }
+    }
+
+    return 0;
+}
