56f5c7756e30e3c83a8bb9e78bcf957422ecd707
Madhav <mega89956@gmail.com>
2025-04-01 02:10:26 +0530

    Solution to Find the Majority Element in an Array

--- Find the Majority Element in an Array@@676c02002dbc8c3e7a280b76/code.c
+++ Find the Majority Element in an Array@@676c02002dbc8c3e7a280b76/code.c
@@ -1,14 +1,34 @@
 #include <stdio.h>
+
 void bubbleSort(int arr[], int size) {
-    for(int i = 0; i < size - 1; i++) {
-        for(int j = 0; j < size - i - 1; j++) { // reducing the search space 
-            if(arr[j] > arr[j+1]) {
-                arr[j] = arr[j] ^ arr[j+1];
-                arr[j+1] = arr[j] ^ arr[j+1];
-                arr[j] = arr[j] ^ arr[j+1];
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
             }
         }
     }
+}
+
+int findMajorityElement(int arr[], int n) {
+    int cnt = 1, maxCnt = 1, majority = arr[0];
+
+    for (int i = 1; i < n; i++) {
+        if (arr[i] == arr[i - 1]) {
+            cnt++;
+        } else {
+            cnt = 1;
+        }
+
+        if (cnt > maxCnt) {
+            maxCnt = cnt;
+            majority = arr[i];
+        }
+    }
+
+    return (maxCnt > n / 2) ? majority : -1;
 }
 
 int main() {
@@ -19,18 +39,9 @@ int main() {
     for (int i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
-    bubbleSort(arr , n);
-    int cnt = 1;
-    for(int i = 0; i < n; i++){
-        if(arr[i] == arr[i+1]){
-            cnt++;
-        }
-        if(cnt > n/2){
-            printf("%d" , arr[i]);
-        }
-        else{
-            printf("-1");
-        }
-    }
+
+    bubbleSort(arr, n);
+    printf("%d\n", findMajorityElement(arr, n));
+
     return 0;
 }
