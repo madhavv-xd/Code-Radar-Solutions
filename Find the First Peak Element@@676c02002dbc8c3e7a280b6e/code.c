71db4a81b5b16ac265de55d1fd7dc459d4509541
Madhav <mega89956@gmail.com>
2025-04-01 01:01:56 +0530

    Solution to Find the First Peak Element

--- Find the First Peak Element@@676c02002dbc8c3e7a280b6e/code.c
+++ Find the First Peak Element@@676c02002dbc8c3e7a280b6e/code.c
@@ -9,6 +9,19 @@ int main() {
         scanf("%d", &a[i]);
     }
 
+    int bitonic = 0;
+    for (int i = 0; i < n - 1; i++) {
+        if (a[i] > a[i + 1]) {
+            is_bitonic = 1;
+            break;
+        }
+    }
+
+    if (!bitonic) {
+        printf("-1\n");
+        return 0;
+    }
+
     int l = 0, r = n - 1;
     while (l < r) {
         int mid = l + (r - l) / 2;
