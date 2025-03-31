a89168723a613892e60f76482d66109848e3e4e3
Madhav <mega89956@gmail.com>
2025-04-01 01:23:35 +0530

    Solution to Find the Frequency of Elements in an Array

--- Find the Frequency of Elements in an Array@@676c02002dbc8c3e7a280b73/code.c
+++ Find the Frequency of Elements in an Array@@676c02002dbc8c3e7a280b73/code.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
-
+void bubbleSort(int arr[] , int size){
+    for(int i = 0; i < size - 1; i++){
+        for(int j = 0; j < size - i - 1; j++){ //reducing the search space 
+            if(arr[j] > arr[j+1]){
+                arr[j] = arr[j] ^ arr[j+1];
+                arr[j+1] = arr[j] ^ arr[j+1];
+                arr[j] = arr[j] ^ arr[j+1];
+            }
+        }
+    }
+}
 int main() {
     int n;
     scanf("%d", &n);
@@ -9,6 +19,8 @@ int main() {
     for (int i = 0; i < n; i++) {
         scanf("%d", &a[i]);
     }
+
+    bubbleSort(a , n);
     int cnt  = 0;
     for(int i = 0; i < n-1; i++){
         for(int j = 0; i < n - 1; j++){
@@ -19,6 +31,7 @@ int main() {
                 i = j;
             }
         }
+        printf("%d %d" , arr[i] , cnt);
     }
     return 0;
 }
