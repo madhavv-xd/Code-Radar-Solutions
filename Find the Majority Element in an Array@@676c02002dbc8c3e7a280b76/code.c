f3dfe80ea6a50ea306c5b69e4cc384d440ec9348
Madhav <mega89956@gmail.com>
2025-04-01 01:55:40 +0530

    Solution to Find the Majority Element in an Array

---
+++ Find the Majority Element in an Array@@676c02002dbc8c3e7a280b76/code.c
@@ -0,0 +1,11 @@
+void bubbleSort(int arr[], int size) {
+    for(int i = 0; i < size - 1; i++) {
+        for(int j = 0; j < size - i - 1; j++) { // reducing the search space 
+            if(arr[j] > arr[j+1]) {
+                arr[j] = arr[j] ^ arr[j+1];
+                arr[j+1] = arr[j] ^ arr[j+1];
+                arr[j] = arr[j] ^ arr[j+1];
+            }
+        }
+    }
+}
