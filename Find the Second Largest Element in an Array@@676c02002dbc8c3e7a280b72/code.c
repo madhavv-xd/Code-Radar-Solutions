8da8e89c6a7d6e785a24c9e9135dfc67921b093c
Madhav <mega89956@gmail.com>
2025-04-01 08:42:59 +0530

    Solution to Find the Second Largest Element in an Array

--- Find the Second Largest Element in an Array@@676c02002dbc8c3e7a280b72/code.c
+++ Find the Second Largest Element in an Array@@676c02002dbc8c3e7a280b72/code.c
@@ -1,38 +1,41 @@
 #include <stdio.h>
-void bubbleSort(int arr[], int size) {
-    for(int i = 0; i < size - 1; i++) {
-        for(int j = 0; j < size - i - 1; j++) { // reducing the search space 
-            if(arr[j] > arr[j+1]) {
-                arr[j] = arr[j] ^ arr[j+1];
-                arr[j+1] = arr[j] ^ arr[j+1];
-                arr[j] = arr[j] ^ arr[j+1];
+
+int main() {
+   int n;
+   scanf("%d" , &n);
+
+   int arr[100];
+   for(int i = 0; i < n; i++){
+    scanf("%d" , &arr[i]);
+   }
+
+    // Bubble Sort to sort the array in ascending order
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Swap arr[j] and arr[j+1]
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
             }
         }
     }
-}
 
-int main() {
-    int n;
-    scanf("%d", &n);
+ 
+    int largest = arr[n - 1];
+    int secondLargest = -1;
 
-    int a[n];
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &a[i]);
-    }
-
-    bubbleSort(a, n);
-    int isSame = 1;
-    for (int i = 1; i < n; i++) {
-        if (a[i] != a[0]) {
-            isSame = 0;
+    for (int i = n - 2; i >= 0; i--) {
+        if (arr[i] != largest) {  
+            secondLargest = arr[i];
             break;
         }
     }
 
-    if (isSame) {
+    if (secondLargest == -1) {
         printf("-1");
     } else {
-        printf("%d", a[n-2]); 
+        printf("%d\n", secondLargest);
     }
 
     return 0;
